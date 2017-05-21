cf069a25e1aca2f49cc5f40fe241565c97110cec
togatoga <togasakitogatoga@gmail.com>
2017-05-21 11:13:26 +0900

    Add mult.asm

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -7,3 +7,26 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @i
+  M=1
+  @2
+  M=0
+(LOOP)
+  @i
+  D=M
+  @1
+  D=D-M
+  @END
+  D;JGT
+  @0
+  D=M
+  @2
+  M=D+M
+  @i
+  M=M+1
+  @LOOP
+  0;JMP
+
+(END)
+  @END
+  0;JMP
