6eb85533a9070e6a68ab6fe068e179c322cbc625
togatoga <togasakitogatoga@gmail.com>
2017-05-21 13:17:32 +0900

    Add Fill.asm

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -6,9 +6,100 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(INIT)
+  //init addr
+  @SCREEN
+  D=A
+  @addr
+  M=D
+
+  @KBD
+  D=M
+  @BLACK
+  D;JGT
+  @WHITE
+  D;JEQ
+
+  @PAINT
+  0;JMP
+
+(BLACK)
+  @color
+  M=-1
+
+  @PAINT
+  0;JMP
+
+(WHITE)
+  @color
+  M=0
+
+  @PAINT
+  0;JMP
+
+(CHECKBLACK)
+  @KBD
+  D=M
+  @INIT
+  D;JEQ
+
+  @addr
+  M=M+1
+
+  @8192
+  D=A
+  @SCREEN
+  D=D+A
+  @addr
+  D=D-M
+
+  @PAINT
+  D;JGT
+
+  @INIT
+  0;JMP
+
+(CHECKWHITE)
+  @KBD
+  D=M
+  @INIT
+  D;JGT
+
+  @addr
+  M=M+1
+
+  @8192
+  D=A
+  @SCREEN
+  D=D+A
+  @addr
+  D=D-M
+
+  @PAINT
+  D;JGT
+
+  @INIT
+  0;JMP
+
+(PAINT)
+  //fill
+  @color
+  D=M
+  @addr
+  A=M
+  M=D
+
+  //check continuous color
+  @color
+  D=M
+  @CHECKBLACK
+  D;JLT
+  @CHECKWHITE
+  D;JEQ
